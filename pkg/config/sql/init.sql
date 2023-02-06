0d15b418699301ad5bcd83fdf034b00366aa7ce6
wjf222 <jffwang@qq.com>
2023-02-06 21:28:52 +0800

    fix: "修改init.sql的错误,删除好友相关idl"

---
+++ pkg/config/sql/init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS douyin;
+
+Use douyin;
+
+-- 用户表
+CREATE TABLE IF NOT EXISTS User
+(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    user_name varchar(64),
+    avatar varchar(64)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 关注列表
+CREATE Table IF NOT EXISTS Relation(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    from_user int,
+    to_user int,
+    create_time timestamp,
+    UNIQUE KEY `follow`(`from_user`,`to_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
